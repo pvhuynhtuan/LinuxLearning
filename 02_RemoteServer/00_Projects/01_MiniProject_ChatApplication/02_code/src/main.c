48b1a865ac05775664c4cd591107013a0c024606
tuanphan <pvhuynhtuan@gmail.com>
2025-03-29 23:32:11 +0700

    remove unused code

--- 02_RemoteServer/00_Projects/01_MiniProject_ChatApplication/02_code/src/main.c
+++ 02_RemoteServer/00_Projects/01_MiniProject_ChatApplication/02_code/src/main.c
@@ -142,33 +142,6 @@ int main(int argc, char *argv[])
             //CA_help();
         }
     }
-    
-    /* int port_no, len, opt;
-    int server_fd, new_socket_fd;
-    struct sockaddr_in serv_addr, client_addr;
-    char buffer[256];
-
-    server_fd = socket(AF_INET, SOCK_STREAM, 0);
-
-    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
-
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(PORT);
-    serv_addr.sin_addr.s_addr = INADDR_ANY;
-
-    if (bind(server_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
-        perror("Bind failed. Error");
-
-    if (listen(server_fd, LISTEN_BACKLOG) == -1)
-        printf("Listen failed\n");
-
-    new_socket_fd = accept(server_fd, (struct sockaddr *)&client_addr, (socklen_t *)&len);
-
-    if (new_socket_fd == -1)
-        printf("accept failed\n");
-    printf("client_addr.sin_addr.s_addr = %d\n",client_addr.sin_addr.s_addr);
-    read(new_socket_fd, buffer, sizeof(buffer));
-    printf("Message received: %s\n", buffer); */
     return 1;
 }
 
